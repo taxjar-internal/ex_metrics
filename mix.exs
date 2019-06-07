4837f5bc429c726212d9ce7958e1db1761ec700c
Matt <matt@lob.com>
2019-06-07 11:39:54 -0700

    chore(base): add project base (#1)

--- mix.exs
+++ mix.exs
@@ -1,13 +1,30 @@
 defmodule Metrix.MixProject do
   use Mix.Project
 
+  @github_url "https://github.com/mpiercy827/metrix"
+
   def project do
     [
       app: :metrix,
+      name: "metrics",
+      description: "Another Elixir metrics library.",
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: @github_url,
+      homepage_url: @github_url,
+      docs: [
+        main: "readme",
+        extras: ["README.md"]
+      ],
+      package: package(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.travis": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -21,8 +38,22 @@ defmodule Metrix.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.19.0", only: :dev, runtime: false},
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.11.1", only: :test},
+      {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false},
+      {:statix, "~> 1.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      files: ~w(mix.exs lib LICENSE* README.md CHANGELOG.md),
+      maintainers: ["Matt Piercy"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => @github_url
+      }
     ]
   end
 end
